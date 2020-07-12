c584b1efbe55ec9617c31a927a6b8b1f5d0f4fdb
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2020-07-12 11:07:53 +0100

    Updates

---
+++ precache-manifest.004435d43b98ea772cb38132247cba94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e76f98c81f3a4fe33652f239a0ccb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753e34aff74692c0fbb5",
+    "url": "/static/css/main.79683249.chunk.css"
+  },
+  {
+    "revision": "105fe60626ce6444394b",
+    "url": "/static/js/2.ee122cd7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.ee122cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753e34aff74692c0fbb5",
+    "url": "/static/js/main.1d64032a.chunk.js"
+  },
+  {
+    "revision": "9fe1e2fc4f608a1af71e",
+    "url": "/static/js/runtime-main.d6463fc5.js"
+  }
+]);
