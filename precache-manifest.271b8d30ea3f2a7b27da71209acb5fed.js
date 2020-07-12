3600d2bc4084585626b9bcb6dc4a8c29a1cf8bee
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2020-07-12 11:18:57 +0100

    Updates

---
+++ precache-manifest.271b8d30ea3f2a7b27da71209acb5fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4747f1db54cb476dbbcd2fef9cb457",
+    "url": "/foodjoint/index.html"
+  },
+  {
+    "revision": "753e34aff74692c0fbb5",
+    "url": "/foodjoint/static/css/main.79683249.chunk.css"
+  },
+  {
+    "revision": "105fe60626ce6444394b",
+    "url": "/foodjoint/static/js/2.ee122cd7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/foodjoint/static/js/2.ee122cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753e34aff74692c0fbb5",
+    "url": "/foodjoint/static/js/main.1d64032a.chunk.js"
+  },
+  {
+    "revision": "99cae5cb9781e053f892",
+    "url": "/foodjoint/static/js/runtime-main.05b8c10e.js"
+  }
+]);
